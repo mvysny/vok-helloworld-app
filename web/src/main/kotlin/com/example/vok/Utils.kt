154208d2845448feb938ca9ecf6733a01ed20e5d
Martin Vysny <vysny@baka.sk>
2018-05-08 21:01:05 +0300

    Implemented confirmDialog

---
+++ web/src/main/kotlin/com/example/vok/Utils.kt
@@ -0,0 +1,28 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.flow.*
+import com.vaadin.flow.component.dialog.Dialog
+
+/**
+ * Shows a simple yes-no confirmation dialog, with given [text] and [title]
+ * @param text defaults to "Are you sure?"
+ */
+fun confirmDialog(text: String = "Are you sure?", title: String? = null, yesListener: ()->Unit) {
+    val window = Dialog()
+    window.apply {
+        setSizeUndefined()
+
+        if (title != null) h2(title)
+        text(text)
+        horizontalLayout {
+            button("Yes") {
+                onLeftClick { yesListener(); window.close() }
+                setPrimary()
+            }
+            button("No") {
+                onLeftClick { window.close() }
+            }
+        }
+    }
+    window.open()
+}
