f02f6b0d2478dd68a0f93b220ad775f249c6c0b9
Martin Vysny <mavi@vaadin.com>
2018-05-08 17:57:34 +0300

    Simplified WelcomeView

--- web/src/main/kotlin/com/example/vok/WelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/WelcomeView.kt
@@ -8,17 +8,13 @@ import com.vaadin.flow.server.Version
 @Route("", layout = MainLayout::class)
 class WelcomeView: VerticalLayout() {
     init {
-        setSizeFull(); content { align(center, middle) }
-        isMargin = false
-        verticalLayout {
-            content { align(center, top) }
-            isMargin = false; isSpacing = true
-            h1("Yay! You're on Vaadin-on-Kotlin!")
-            image("images/chucknorris.jpg")
-            div { html("<strong>Vaadin Server version: </strong> ${Version.getFullVersion()}") }
-            div { html("<strong>Kotlin version: </strong> ${KotlinVersion.CURRENT}") }
-            div { html("<strong>JVM version: </strong> $jvmVersion") }
-        }
+        setSizeFull(); content { align(center, middle) }; isMargin = false; isSpacing = true
+
+        h1("Yay! You're on Vaadin-on-Kotlin!")
+        image("images/chucknorris.jpg")
+        div { html("<strong>Vaadin Server version: </strong> ${Version.getFullVersion()}") }
+        div { html("<strong>Kotlin version: </strong> ${KotlinVersion.CURRENT}") }
+        div { html("<strong>JVM version: </strong> $jvmVersion") }
     }
 }
 
