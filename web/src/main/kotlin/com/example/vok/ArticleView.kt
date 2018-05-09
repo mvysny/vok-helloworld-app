c39b52d02a34e288bbc44d49f0623aa27946a025
Martin Vysny <mavi@vaadin.com>
2018-05-09 17:42:25 +0300

    ArticleView: added comments

--- web/src/main/kotlin/com/example/vok/ArticleView.kt
+++ web/src/main/kotlin/com/example/vok/ArticleView.kt
@@ -3,14 +3,20 @@ package com.example.vok
 import com.github.vok.karibudsl.flow.*
 import com.github.vokorm.getById
 import com.vaadin.flow.component.*
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.*
 
 @Route("article")
 class ArticleView: VerticalLayout(), HasUrlParameter<Long> {
+    private lateinit var article: Article
     private val editLink: RouterLink
     private lateinit var title: Text
     private lateinit var text: Text
+    private val comments: Div
+    private val commentBinder = beanValidationBinder<Comment>()
+    private lateinit var createComment: Button
     init {
         div {
             strong("Title: ")
@@ -20,15 +26,43 @@ class ArticleView: VerticalLayout(), HasUrlParameter<Long> {
             strong("Text: ")
             this@ArticleView.text = text("")
         }
+        p("Comments")
+        comments = div()
+        p("Add a comment:")
+        textField("Commenter:") {
+            bind(commentBinder).bind(Comment::commenter)
+        }
+        textField("Body:") {
+            bind(commentBinder).bind(Comment::body)
+        }
+        createComment = button("Create") {
+            onLeftClick { createComment() }
+        }
         editLink = routerLink(null, "Edit")
         routerLink(text = "Back", viewType = ArticlesView::class)
     }
 
     override fun setParameter(event: BeforeEvent, articleId: Long?) {
-        val article = Article.getById(articleId!!)
+        article = Article.getById(articleId!!)
         title.text = article.title
         text.text = article.text
         editLink.setRoute(EditArticleView::class, articleId)
+    }
+
+    private fun createComment() {
+        val comment = Comment()
+        if (commentBinder.validate().isOk && commentBinder.writeBeanIfValid(comment)) {
+            comment.article_id = article.id
+            comment.save()
+            refreshComments()
+            commentBinder.readBean(Comment())  // this clears the comment fields
+        }
+    }
+    private fun refreshComments() {
+        comments.removeAll()
+        article.comments.getAll().forEach { comment ->
+            comments.html("<p><strong>Commenter:</strong>${comment.commenter}</p><p><strong>Comment:</strong>${comment.body}</p>")
+        }
     }
 
     companion object {
