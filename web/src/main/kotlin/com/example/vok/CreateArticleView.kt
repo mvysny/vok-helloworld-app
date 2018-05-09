b222e3aaffc034b9d6b12fa9b5ce16d7aa05ed5f
Martin Vysny <mavi@vaadin.com>
2018-05-09 08:35:00 +0300

    Added CreateArticleView

---
+++ web/src/main/kotlin/com/example/vok/CreateArticleView.kt
@@ -0,0 +1,20 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.flow.*
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.Route
+
+@Route("create-article")
+class CreateArticleView: VerticalLayout() {
+    private val binder = beanValidationBinder<Article>()
+    init {
+        h1("New Article")
+        textField("Title") {
+            bind(binder).bind(Article::title)
+        }
+        textArea("Text") {
+            bind(binder).bind(Article::text)
+        }
+        button("Save Article")
+    }
+}
