14850d271aafbabcc412f6659693d711c00d9eb1
Martin Vysny <mavi@vaadin.com>
2018-05-09 11:06:37 +0300

    Added ArticleView

---
+++ web/src/main/kotlin/com/example/vok/ArticleView.kt
@@ -0,0 +1,33 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.flow.*
+import com.github.vokorm.getById
+import com.vaadin.flow.component.*
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.*
+
+@Route("article")
+class ArticleView: VerticalLayout(), HasUrlParameter<Long> {
+    private lateinit var title: Text
+    private lateinit var text: Text
+    init {
+        div {
+            strong("Title: ")
+            this@ArticleView.title = text("")
+        }
+        div {
+            strong("Text: ")
+            this@ArticleView.text = text("")
+        }
+    }
+
+    override fun setParameter(event: BeforeEvent, articleId: Long?) {
+        val article = Article.getById(articleId!!)
+        title.text = article.title
+        text.text = article.text
+    }
+
+    companion object {
+        fun navigateTo(articleId: Long) = UI.getCurrent().navigate(ArticleView::class.java, articleId)
+    }
+}
