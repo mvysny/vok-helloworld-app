a416caa736f3b353fde3e3929ea00b919012180b
Martin Vysny <mavi@vaadin.com>
2018-05-09 07:57:07 +0300

    WelcomeView mapped to old-welcome

--- web/src/main/kotlin/com/example/vok/MainLayout.kt
+++ web/src/main/kotlin/com/example/vok/MainLayout.kt
@@ -1,20 +1,15 @@
 package com.example.vok
 
-import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.router.RouterLayout
-import com.vaadin.flow.theme.Theme
-import com.vaadin.flow.theme.lumo.Lumo
 
 /**
  * The main layout.
  */
 @BodySize(width = "100vw", height = "100vh")
-@HtmlImport("frontend://styles.html")
 @Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes")
-@Theme(Lumo::class)
 class MainLayout : VerticalLayout(), RouterLayout {
 
     init {
