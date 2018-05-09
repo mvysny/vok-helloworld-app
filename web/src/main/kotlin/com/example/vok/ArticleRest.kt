fe74ba5368faa4e05002a07407019f19db71a18b
Martin Vysny <mavi@vaadin.com>
2018-05-09 08:26:23 +0300

    Added ArticleRest

---
+++ web/src/main/kotlin/com/example/vok/ArticleRest.kt
@@ -0,0 +1,18 @@
+package com.example.vok
+
+import com.github.vokorm.*
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+
+@Path("/articles")
+class ArticleRest {
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun get(@PathParam("id") id: Long): Article = Article.findById(id) ?: throw NotFoundException("No article with id $id")
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getAll(): List<Article> = Article.findAll()
+}
