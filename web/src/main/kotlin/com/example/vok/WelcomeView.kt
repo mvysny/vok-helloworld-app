64287e5a39388ff5761f02d728b40c87e2e7f875
Martin Vysny <martin@vysny.me>
2019-01-13 10:28:05 +0200

    WelcomeView: port to KComposite

--- web/src/main/kotlin/com/example/vok/WelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/WelcomeView.kt
@@ -6,15 +6,17 @@ import com.vaadin.flow.router.Route
 import com.vaadin.flow.server.Version
 
 @Route("")
-class WelcomeView: VerticalLayout() {
-    init {
-        setSizeFull(); content { align(center, middle) }; isMargin = false; isSpacing = true
+class WelcomeView: KComposite() {
+    private val root = ui {
+        verticalLayout {
+            setSizeFull(); content { align(center, middle) }; isMargin = false; isSpacing = true
 
-        h1("Yay! You're on Vaadin-on-Kotlin!")
-        image("images/chucknorris.jpg")
-        div { html("<strong>Vaadin Server version: </strong> ${Version.getFullVersion()}") }
-        div { html("<strong>Kotlin version: </strong> ${KotlinVersion.CURRENT}") }
-        div { html("<strong>JVM version: </strong> $jvmVersion") }
+            h1("Yay! You're on Vaadin-on-Kotlin!")
+            image("images/chucknorris.jpg")
+            div { html("<strong>Vaadin Server version: </strong> ${Version.getFullVersion()}") }
+            div { html("<strong>Kotlin version: </strong> ${KotlinVersion.CURRENT}") }
+            div { html("<strong>JVM version: </strong> $jvmVersion") }
+        }
     }
 }
 
