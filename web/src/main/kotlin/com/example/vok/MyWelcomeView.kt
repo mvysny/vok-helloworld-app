94b9f449101ad0b1bde7341e5eb5129dc49491e5
Martin Vysny <martin@vysny.me>
2019-01-14 10:47:26 +0200

    stuff ported to KComposite

--- web/src/main/kotlin/com/example/vok/MyWelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/MyWelcomeView.kt
@@ -1,12 +1,11 @@
 package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.*
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 
 @Route("")
-class MyWelcomeView: VerticalLayout() {
-    init {
+class MyWelcomeView: KComposite() {
+    private val root = ui {
         verticalLayout {
             h1("Hello, Vaadin-on-Kotlin!")
             routerLink(text = "My Blog", viewType = ArticlesView::class)
