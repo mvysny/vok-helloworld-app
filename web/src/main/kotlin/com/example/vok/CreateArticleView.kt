adb2f37320e8abf114ac3b560e8bcd65d4e0f5c4
Martin Vysny <martin@vysny.me>
2019-01-14 10:55:58 +0200

    stuff ported to KComposite

--- web/src/main/kotlin/com/example/vok/CreateArticleView.kt
+++ web/src/main/kotlin/com/example/vok/CreateArticleView.kt
@@ -1,16 +1,17 @@
 package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.*
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 
 @Route("create-article")
-class CreateArticleView: VerticalLayout() {
-    private val editor: ArticleEditor
-    init {
-        h1("New Article")
-        editor = articleEditor {
-            article = Article()
+class CreateArticleView: KComposite() {
+    private lateinit var editor: ArticleEditor
+    private val root = ui {
+        verticalLayout {
+            h1("New Article")
+            editor = articleEditor {
+                article = Article()
+            }
         }
     }
 }
