f3c173c87af6354a1a3977c817c4b026c4382247
Martin Vysny <martin@vysny.me>
2019-01-14 10:52:34 +0200

    stuff ported to KComposite

--- web/src/main/kotlin/com/example/vok/EditArticleView.kt
+++ web/src/main/kotlin/com/example/vok/EditArticleView.kt
@@ -2,15 +2,16 @@ package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.*
 import com.github.vokorm.getById
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.*
 
 @Route("edit-article")
-class EditArticleView: VerticalLayout(), HasUrlParameter<Long> {
-    private val editor: ArticleEditor
-    init {
-        h1("Edit Article")
-        editor = articleEditor()
+class EditArticleView: KComposite(), HasUrlParameter<Long> {
+    private lateinit var editor: ArticleEditor
+    private val root = ui {
+        verticalLayout {
+            h1("Edit Article")
+            editor = articleEditor()
+        }
     }
 
     override fun setParameter(event: BeforeEvent, articleId: Long?) {
