e8e479f8f8d3483cdc5bcd15505e4af71224c49c
Martin Vysny <martin@vysny.me>
2019-01-14 10:08:12 +0200

    minor

--- web/src/main/kotlin/com/example/vok/WelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/WelcomeView.kt
@@ -1,7 +1,6 @@
 package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.*
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.server.Version
 
