b6d061234b23036a796ab9cb8a4db7dd20595a8a
Martin Vysny <mavi@vaadin.com>
2019-12-09 18:08:59 +0200

    v bump

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -15,11 +15,11 @@ dependencies {
     // logging
     // currently we are logging through the SLF4J API to LogBack. See logback.xml file for the logger configuration
     compile("ch.qos.logback:logback-classic:1.2.3")
-    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("org.slf4j:slf4j-api:1.7.28")
 
     // db
-    compile("org.flywaydb:flyway-core:5.2.4")
-    compile("com.h2database:h2:1.4.198")
+    compile("org.flywaydb:flyway-core:6.1.0")
+    compile("com.h2database:h2:1.4.200")
 
     // REST
     compile("eu.vaadinonkotlin:vok-rest:${properties["vok_version"]}")
