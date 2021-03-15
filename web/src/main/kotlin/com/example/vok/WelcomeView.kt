c1dfa244b677db1234c083ad95d8d2dbcfa53e2a
Martin Vysny <mavi@vaadin.com>
2021-03-15 08:50:40 +0200

    show Vaadin version instead of Vaadin Flow version

--- web/src/main/kotlin/com/example/vok/WelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/WelcomeView.kt
@@ -1,8 +1,9 @@
 package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.*
+import com.vaadin.flow.component.dependency.NpmPackage
 import com.vaadin.flow.router.Route
-import com.vaadin.flow.server.Version
+import com.vaadin.shrinkwrap.VaadinCoreShrinkWrap
 
 @Route("")
 class WelcomeView: KComposite() {
@@ -12,7 +13,7 @@ class WelcomeView: KComposite() {
 
             h1("Yay! You're on Vaadin-on-Kotlin!")
             image("images/chucknorris.jpg")
-            div { html("<strong>Vaadin Server version: </strong> ${Version.getFullVersion()}") }
+            div { html("<strong>Vaadin version: </strong> $vaadinVersion") }
             div { html("<strong>Kotlin version: </strong> ${KotlinVersion.CURRENT}") }
             div { html("<strong>JVM version: </strong> $jvmVersion") }
         }
@@ -20,3 +21,8 @@ class WelcomeView: KComposite() {
 }
 
 val jvmVersion: String get() = System.getProperty("java.version")
+val vaadinVersion: String get() {
+    val annotation: NpmPackage = checkNotNull(VaadinCoreShrinkWrap::class.java.getAnnotation(NpmPackage::class.java),
+        { "We only support Vaadin 14 and higher" })
+    return annotation.version
+}
