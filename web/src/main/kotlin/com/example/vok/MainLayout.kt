66b582e9b91df8cbd87422961cc8e018d6abe025
Martin Vysny <mavi@vaadin.com>
2021-03-15 08:59:50 +0200

    Fix MainLayout to fill the browser view

--- web/src/main/kotlin/com/example/vok/MainLayout.kt
+++ web/src/main/kotlin/com/example/vok/MainLayout.kt
@@ -1,6 +1,7 @@
 package com.example.vok
 
 import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.router.BeforeEnterEvent
 import com.vaadin.flow.router.BeforeEnterObserver
 import com.vaadin.flow.router.RouterLayout
@@ -12,7 +13,12 @@ import eu.vaadinonkotlin.vaadin10.VokSecurity
  * * provides application frame around all views
  * * handles redirect to the [LoginView] if no user is logged in.
  */
+@Viewport(Viewport.DEVICE_DIMENSIONS)
 class MainLayout : Div(), RouterLayout, BeforeEnterObserver {
+    init {
+        setSizeFull()
+    }
+
     override fun beforeEnter(event: BeforeEnterEvent) {
         if (event.navigationTarget != LoginView::class.java && !Session.loginService.isLoggedIn) {
             event.rerouteTo(LoginView::class.java)
