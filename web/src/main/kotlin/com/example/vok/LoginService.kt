0bef3d88659e8449c07789fb75675274afb5ee60
Martin Vysny <mavi@vaadin.com>
2021-10-08 16:38:33 +0300

    add protection for session fixation attack

--- web/src/main/kotlin/com/example/vok/LoginService.kt
+++ web/src/main/kotlin/com/example/vok/LoginService.kt
@@ -1,6 +1,8 @@
 package com.example.vok
 
 import com.vaadin.flow.component.UI
+import com.vaadin.flow.server.VaadinRequest
+import com.vaadin.flow.server.VaadinService
 import com.vaadin.flow.server.VaadinSession
 import eu.vaadinonkotlin.vaadin10.Session
 import java.io.Serializable
@@ -11,6 +13,12 @@ class LoginService : Serializable {
 
     fun login(username: String, password: String): Boolean {
         currentUser = User(username)
+
+        // creates a new session after login, to prevent session fixation attack
+        VaadinService.reinitializeSession(VaadinRequest.getCurrent())
+
+        // this will cause the UI to be re-created. Since the user is now logged in and present in the session,
+        // the UI should now initialize properly and should not show the LoginView.
         UI.getCurrent().page.reload()
         return true
     }
@@ -19,8 +27,9 @@ class LoginService : Serializable {
         private set
 
     fun logout() {
-        VaadinSession.getCurrent().close()
-        UI.getCurrent().navigate("")
+        Session.current.close()
+        // The UI is recreated by the page reload, and since there is no user in the session (since it has been cleared),
+        // the UI will show the LoginView.
         UI.getCurrent().page.reload()
     }
 
