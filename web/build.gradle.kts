d8700e64512148e8029772f60a7f76036e9e1da3
Martin Vysny <mavi@vaadin.com>
2022-06-15 17:49:58 +0300

    vaadin v bump

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -21,12 +21,12 @@ dependencies {
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:1.7.32")
-    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+    implementation("org.slf4j:slf4j-api:1.7.36")
 
     // db
-    implementation("org.flywaydb:flyway-core:8.4.1")
-    implementation("com.h2database:h2:2.1.210")
+    implementation("org.flywaydb:flyway-core:8.5.12")
+    implementation("com.h2database:h2:2.1.212")
 
     // REST
     implementation("eu.vaadinonkotlin:vok-rest:${properties["vok_version"]}")
@@ -35,6 +35,3 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
-vaadin {
-//    nodeVersion = "v16.13.2" // workaround for https://github.com/vaadin/flow/issues/12732
-}
