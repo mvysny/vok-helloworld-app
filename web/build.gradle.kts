bf14b2b89874ea1740b9bba86028453d3c4b0121
Martin Vysny <mavi@vaadin.com>
2022-06-15 17:51:13 +0300

    dependency v-bump

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.11")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.15")
     testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.24")
 }
 
