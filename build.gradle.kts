34c572e77f5eda408c6ec8557d0921a5bac19292
Martin Vysny <mavi@vaadin.com>
2022-11-08 20:26:27 +0200

    removed the 'web' subproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,36 +3,56 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("com.vaadin") version "23.2.7" apply(false)
+    id("com.vaadin") version "23.2.7"
+    application
 }
 
 defaultTasks("clean", "build")
 
-allprojects {
-    group = "com.example.vok"
-    version = "1.0-SNAPSHOT"
-    repositories {
-        mavenCentral()
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Vaadin
+    implementation("eu.vaadinonkotlin:vok-framework-vokdb:${properties["vok_version"]}")
+    implementation("com.vaadin:vaadin-core:${properties["vaadin_version"]}")
+    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:10.1")
+
+    implementation("com.zaxxer:HikariCP:5.0.1")
+
+    // logging
+    // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
+    implementation("org.slf4j:slf4j-simple:2.0.0")
+
+    // db
+    implementation("org.flywaydb:flyway-core:9.5.1")
+    implementation("com.h2database:h2:2.1.214")
+
+    // REST
+    implementation("eu.vaadinonkotlin:vok-rest:${properties["vok_version"]}")
+
+    // Kotlin
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exception stacktraces of failed tests in CI
+        exceptionFormat = TestExceptionFormat.FULL
     }
 }
 
-subprojects {
-    apply { plugin("kotlin") }
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        testLogging {
-            // to see the exceptions of failed tests in Travis-CI console.
-            exceptionFormat = TestExceptionFormat.FULL
-        }
-    }
+application {
+    mainClass.set("com.example.vok.MainKt")
 }
