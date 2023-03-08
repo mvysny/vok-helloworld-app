eb5cf2355142786d29d22c68f7a73fea7806acf3
Martin Vysny <mavi@vaadin.com>
2023-03-08 20:24:10 +0200

    Remove invalid annotation from MainLayout

--- src/main/kotlin/com/example/vok/MainLayout.kt
+++ src/main/kotlin/com/example/vok/MainLayout.kt
@@ -2,7 +2,6 @@ package com.example.vok
 
 import com.github.mvysny.karibudsl.v10.KComposite
 import com.github.mvysny.karibudsl.v10.div
-import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.router.BeforeEnterEvent
 import com.vaadin.flow.router.BeforeEnterObserver
 import com.vaadin.flow.router.RouterLayout
@@ -13,7 +12,6 @@ import eu.vaadinonkotlin.vaadin.Session
  * * provides application frame around all views
  * * handles redirect to the [LoginView] if no user is logged in.
  */
-@Viewport(Viewport.DEVICE_DIMENSIONS)
 class MainLayout : KComposite(), RouterLayout, BeforeEnterObserver {
     private val root = ui {
         div {
