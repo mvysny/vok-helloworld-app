3d48e6939ded40fc0673b8288d2b0d6fda50b6ec
Martin Vysny <mavi@vaadin.com>
2024-09-26 11:56:41 +0300

    modernize

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -46,7 +47,7 @@ java {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    compilerOptions.jvmTarget = JvmTarget.JVM_17
 }
 
 tasks.withType<Test> {
