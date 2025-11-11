0557e926c6e8313c46ef898559cba85bc9fa7e5f
Martin Vysny <mavi@vaadin.com>
2025-11-11 11:19:26 +0200

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.2.20"
     alias(libs.plugins.vaadin)
     application
 }
